f3b7680b75991adceb0a33fe288d6e9accdcb7cf
aditya-rathore15 <rathoreaditya.work@gmail.com>
2025-12-20 08:16:11 +0530

    Print numbers in reverse

---
+++ src/basic/Lecture1Print1ToNReverse.java
@@ -0,0 +1,22 @@
+package basic;
+
+import java.util.Scanner;
+
+public class Lecture1Print1ToNReverse {
+
+    private static void print(int n) {
+        if(n == 0) {
+            return;
+        }
+
+        System.out.println(n);
+        print(n-1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+        print(n);
+    }
+}
